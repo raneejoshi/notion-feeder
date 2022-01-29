5189a770699713bd8367e8bbc661b20db098960e
raneejoshi <raneejoshi@gmail.com>
2022-01-29 15:23:21 +0800

    Update notion.js

--- src/notion.js
+++ src/notion.js
@@ -30,7 +30,7 @@ export async function getFeedUrlsFromNotion() {
           },
           {
             property: 'Source',
-            equals: 'RSS',
+            select: {equals: 'RSS'},
           },
         ],
       },
