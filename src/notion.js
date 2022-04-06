7f812ee4375434f12cbf365bd088d30a0968f407
raneejoshi <raneejoshi@gmail.com>
2022-04-07 06:41:48 +0800

    Update notion.js

--- src/notion.js
+++ src/notion.js
@@ -81,7 +81,7 @@ export async function addFeedItemToNotion(notionItem) {
         Series: {
           multi_select: [{ name: 'Formula 1' }],
         },
-        Link: {
+        URL: {
           url: link,
         },
       },
@@ -117,7 +117,7 @@ export async function deleteOldUnreadFeedItemsFromNotion() {
             },
           },
           {
-            property: 'Read',
+            property: 'Read?',
             checkbox: {
               equals: false,
             },
